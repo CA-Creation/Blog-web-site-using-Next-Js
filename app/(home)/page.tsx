9961a862bf2f64465e7bd67da202d5e408108a07
Chamodya B Herath <134180331+CA-Creation@users.noreply.github.com>
2025-05-22 00:20:04 +0530

    done Hero Section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero-section";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
     </div>
   );
 }
--- components/home/header/search-input.tsx
+++ components/home/header/search-input.tsx
@@ -1,8 +1,17 @@
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
 import React from 'react'
 
 const SearchInput = () => {
   return (
-    <div>SearchInput</div>
+    <form>
+        < div className='relative'>
+            <Search className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground'></Search>
+            <Input type='text' name='search' placeholder='Search articles...' className='pl-10 w-48 focus-visible:ring-1'>
+
+            </Input>
+        </div>
+    </form>
   )
 }
 
